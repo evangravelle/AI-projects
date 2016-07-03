f6a9e189ec4f8bf19a9a9667abb829a79b06fa41
Evan Gravelle <evangravelle@gmail.com>
2016-07-02 22:30:09 -0700

    blah

--- OpenAIGym/cartpole.py
+++ OpenAIGym/cartpole.py
@@ -1,6 +1,12 @@
 import gym
-env = gym.make('CartPole-v0')
+# env = gym.make('CartPole-v0')
+env = gym.make('MountainCar-v0')
+env.monitor.start('./tmp/cartpole-experiment-1', force=True)
 env.reset()
 for _ in range(1000):
     env.render()
-    env.step(env.action_space.sample())  # take a random action
+    observation, reward, done, info = env.step(env.action_space.sample())  # take a random action
+    if done:
+        break
+
+env.monitor.close()
