40eb4e03f0f727c2e51b8cb87a4ded83a2739175
Evan Gravelle <evangravelle@gmail.com>
2016-08-11 22:59:22 -0700

    NN started

--- OpenAIGym/pong.py
+++ OpenAIGym/pong.py
@@ -11,6 +11,7 @@
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 # Initializations
 env = gym.make('Pong-v0')
@@ -26,13 +27,11 @@ epsilon_final = 0.1
 num_episodes = 1
 num_timesteps = 100
 
-epsilon_coefficient = (epsilon - epsilon_final) ** (1. / num_episodes)
-ep_length = np.zeros(num_episodes)
-np.set_printoptions(precision=2)
-
 
 def reduce_image(_obs):
     new_obs = np.sum(_obs, 2) / (3. * 256.)
+    new_obs[new_obs < .5] = 0
+    new_obs[new_obs >= .5] = 1
     return new_obs
 
 
@@ -44,6 +43,51 @@ def epsilon_greedy(_epsilon, _vals):
     else:
         _action = env.action_space.sample()
     return int(_action)
+
+
+epsilon_coefficient = (epsilon - epsilon_final) ** (1. / num_episodes)
+ep_length = np.zeros(num_episodes)
+np.set_printoptions(precision=2)
+
+sess = tf.InteractiveSession()
+# assume there is an observation "obs"
+obs_reduced = reduce_image(obs)
+x = -tf.nn.max_pool(-obs_reduced, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+x = tf.placeholder(tf.float32, shape=[None, num_rows*num_cols/4.])  # 1st dim is batch size
+y_ = tf.placeholder(tf.float32, shape=[None, num_actions])
+
+W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], mean=0.0, stddev=0.1))
+b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+x_image = tf.reshape(x, [-1, num_rows/2., num_cols/2., 1])
+h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], mean=0.0, stddev=0.1))
+b_conv2 = tf.Variable(tf.constant(0.1, shape=[32]))
+h_conv2 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+# Fully connected layer, with arbitrary choice of 1024 neurons
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# Output layer with dropout
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess.run(tf.initialize_all_variables())
+
+start_time = datetime.datetime.now().time()
 
 
 # Training loop
--- TensorFlowTests/MNIST_better.py
+++ TensorFlowTests/MNIST_better.py
@@ -13,12 +13,12 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(_x, W):
+    return tf.nn.conv2d(_x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+def max_pool_2x2(_x):
+    return tf.nn.max_pool(_x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
