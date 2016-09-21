66291e0326bf054fa02f0cdf0c936caf8e38c004
Evan Gravelle <evangravelle@gmail.com>
2016-09-21 01:29:22 -0700

    more exercises

---
+++ thinkpython/exercise15.py
@@ -0,0 +1,22 @@
+# Some classes practice
+
+class Point(object):
+    """A point."""
+    def __init__(self, x=0., y=0.):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+
+
+
+class Rectangle(object):
+    """Consists of top left and lower right point."""
+    def __init__(self, ):
+        self.ll = x
+        self.ur = y
+
+
+def print_attributes(obj):
+    for attr in obj.__dict__:
+        print attr, getattr(obj, attr)
