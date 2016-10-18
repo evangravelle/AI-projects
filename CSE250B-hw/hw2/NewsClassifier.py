55c9a020660c7e93c2b86617904e25d75718d9d7
Evan Gravelle <evangravelle@gmail.com>
2016-10-18 00:15:55 -0700

    added hw2

--- CSE250B-hw/hw2/NewsClassifier.py
+++ CSE250B-hw/hw2/NewsClassifier.py
@@ -3,24 +3,61 @@
 # is chosen, and performance as a function of number of prototypes is analyzed.
 
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
+num_proto = 5000
+word_thresh = 0
 num_words = 61188
 num_train_docs = 11269
 num_test_docs = 7505
 num_labels = 20
 
 train_counts = np.zeros((num_train_docs, num_words), dtype=np.uint16)
+train_labels = np.zeros(num_train_docs, dtype=np.uint16)
 test_counts = np.zeros((num_test_docs, num_words), dtype=np.uint16)
+test_labels = np.zeros(num_test_docs, dtype=np.uint16)
+train_label_prob = np.zeros(num_train_docs, dtype=np.float32)
+train_word_prob = np.zeros((num_train_docs, num_words), dtype=np.float32)
+
+train_lengths = np.sum(train_counts, axis=1)
 
 # Save the data for the prototype set
-with open('20news-bydate/train.data') as train_file:
-    for line in train_file:
+with open('20news-bydate/train.data') as train_data_file:
+    for line in train_data_file:
         doc, word, count = [int(i) for i in line.split()]
-        train_counts[doc, word] = count
+        train_counts[doc - 1, word - 1] = count
 
-with open('20news-bydate/test.data') as test_file:
-    for line in test_file:
+with open('20news-bydate/train.label') as train_label_file:
+    for index, line in enumerate(train_label_file):
+        train_labels[index] = int(line) - 1
+
+with open('20news-bydate/test.data') as test_data_file:
+    for line in test_data_file:
         doc, word, count = [int(i) for i in line.split()]
-        test_counts[doc, word] = count
+        test_counts[doc - 1, word - 1] = count
 
+with open('20news-bydate/test.label') as test_label_file:
+    for index, line in enumerate(test_label_file):
+        test_labels[index] = int(line) - 1
+
+# Finds marginal probability of each class
+for doc in xrange(num_train_docs):
+    train_label_prob[train_labels[doc]] += 1.
+train_label_prob /= num_train_docs
+
+# Finds probability of each word in each class
+for doc in xrange(num_train_docs):
+    train_word_prob[doc, :] = ((train_counts[doc, :] + np.ones(num_words)) /
+                            (np.sum(train_counts[doc, :]) + num_words))
+
+scores = np.zeros((num_test_docs, num_labels), dtype=np.float32)
+for doc in xrange(num_test_docs):
+    if doc % 100 == 0:
+        print doc
+    for label in xrange(num_labels):
+        scores[doc, label] = (np.log(train_label_prob[label]) +
+                              np.sum(test_counts[doc, :] * np.log(train_word_prob[doc, :])))
+
+test_predictions = np.argmax(scores, axis=1)
+accuracy = np.sum(np.equal(test_predictions, test_labels)) / float(num_test_docs)
+print 'accuracy = ', accuracy
