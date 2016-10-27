a17ffb95c86af8169d733579e9e78a2b0e3654cb
Evan Gravelle <evangravelle@gmail.com>
2016-10-26 20:10:04 -0700

    works

--- CSE250B-hw/hw2/NewsClassifier.py
+++ CSE250B-hw/hw2/NewsClassifier.py
@@ -12,33 +12,34 @@ num_train_docs = 11269
 num_test_docs = 7505
 num_labels = 20
 
-train_counts = np.zeros((num_train_docs, num_words), dtype=np.uint16)
+train_counts = np.zeros((num_labels, num_words), dtype=np.uint16)
 train_labels = np.zeros(num_train_docs, dtype=np.uint16)
 test_counts = np.zeros((num_test_docs, num_words), dtype=np.uint16)
 test_labels = np.zeros(num_test_docs, dtype=np.uint16)
-train_label_prob = np.zeros(num_labels, dtype=np.float32)
-train_word_prob = np.zeros((num_train_docs, num_words), dtype=np.float32)
+train_label_prob = np.zeros(num_labels, dtype=np.float64)
+train_word_prob = np.zeros((num_labels, num_words), dtype=np.float64)
 
 train_lengths = np.sum(train_counts, axis=1)
 
 # Save the data for the prototype set
-with open('20news-bydate/train.data') as train_data_file:
-    for line in train_data_file:
-        doc, word, count = [int(i) for i in line.split()]
-        train_counts[doc - 1, word - 1] = count
-
 with open('20news-bydate/train.label') as train_label_file:
     for index, line in enumerate(train_label_file):
         train_labels[index] = int(line) - 1
+
+with open('20news-bydate/train.data') as train_data_file:
+    for line in train_data_file:
+        doc, word, count = [int(i) for i in line.split()]
+        label = train_labels[doc - 1]
+        train_counts[label, word - 1] += count
+
+with open('20news-bydate/test.label') as test_label_file:
+    for index, line in enumerate(test_label_file):
+        test_labels[index] = int(line) - 1
 
 with open('20news-bydate/test.data') as test_data_file:
     for line in test_data_file:
         doc, word, count = [int(i) for i in line.split()]
         test_counts[doc - 1, word - 1] = count
-
-with open('20news-bydate/test.label') as test_label_file:
-    for index, line in enumerate(test_label_file):
-        test_labels[index] = int(line) - 1
 
 # Finds marginal probability of each class
 for doc in xrange(num_train_docs):
@@ -46,18 +47,18 @@ for doc in xrange(num_train_docs):
 train_label_prob /= num_train_docs
 
 # Finds probability of each word in each class
-for doc in xrange(num_train_docs):
-    train_word_prob[doc, :] = ((train_counts[doc, :] + np.ones(num_words)) /
-                               (np.sum(train_counts[doc, :]) + num_words))
+for label in xrange(num_labels):
+    train_word_prob[label, :] = ((train_counts[label, :] + np.ones(num_words, dtype=np.uint16)) /
+                                 float(np.sum(train_counts[label, :]) + num_words))
 
-scores = np.zeros((num_test_docs, num_labels), dtype=np.float32)
+scores = np.zeros((num_test_docs, num_labels), dtype=np.float64)
 for doc in xrange(num_test_docs):
     if doc % 100 == 0:
         print doc
     for label in xrange(num_labels):
         scores[doc, label] = (np.log(train_label_prob[label]) +
-                              np.sum(test_counts[doc, :] * np.log(train_word_prob[doc, :])))
-
+                              np.sum(test_counts[doc, :] * np.log(train_word_prob[label, :])))
+print 'scores[0, :] = ', scores[0, :]
 
 entropies = -np.sum(train_word_prob * np.log(train_word_prob), axis=0)
 sorted_words1 = np.argsort(entropies)
@@ -67,10 +68,13 @@ variances = np.var(train_word_prob, axis=0)
 sorted_words2 = np.argsort(-variances)
 sorted_words2 = sorted_words2[0:num_proto]
 
-# Calculates performace of prototypes
+# Calculates performance of prototypes
 test_predictions = np.argmax(scores, axis=1)
 accuracy = np.sum(np.equal(test_predictions, test_labels)) / float(num_test_docs)
+print 'test_predictions[0:500] = ', test_predictions[0:500]
+print 'test_labels[0:500] = ', test_labels[0:500]
 print 'accuracy = ', accuracy
 
 # Prints a representative set of words in each class, by taking the words
 # with highest probability in each class from the set of prototypes
+reps = 0
