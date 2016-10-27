ae2c524a50f952334f94e1bd87c10147cc8cdc19
Evan Gravelle <evangravelle@gmail.com>
2016-10-27 10:00:05 -0700

    d

--- CSE250B-hw/hw2/NewsClassifier.py
+++ CSE250B-hw/hw2/NewsClassifier.py
@@ -11,6 +11,7 @@ num_words = 61188
 num_train_docs = 11269
 num_test_docs = 7505
 num_labels = 20
+num_reps = 10
 
 train_counts = np.zeros((num_labels, num_words), dtype=np.uint16)
 train_labels = np.zeros(num_train_docs, dtype=np.uint16)
@@ -18,6 +19,8 @@ test_counts = np.zeros((num_test_docs, num_words), dtype=np.uint16)
 test_labels = np.zeros(num_test_docs, dtype=np.uint16)
 train_label_prob = np.zeros(num_labels, dtype=np.float64)
 train_word_prob = np.zeros((num_labels, num_words), dtype=np.float64)
+label_dict = dict()
+vocab_dict = dict()
 
 train_lengths = np.sum(train_counts, axis=1)
 
@@ -32,6 +35,11 @@ with open('20news-bydate/train.data') as train_data_file:
         label = train_labels[doc - 1]
         train_counts[label, word - 1] += count
 
+with open('20news-bydate/train.map') as train_map_file:
+    for line in train_map_file:
+        label_name, label = [i for i in line.split()]
+        label_dict[int(label) - 1] = label_name
+
 with open('20news-bydate/test.label') as test_label_file:
     for index, line in enumerate(test_label_file):
         test_labels[index] = int(line) - 1
@@ -40,6 +48,10 @@ with open('20news-bydate/test.data') as test_data_file:
     for line in test_data_file:
         doc, word, count = [int(i) for i in line.split()]
         test_counts[doc - 1, word - 1] = count
+
+with open('vocabulary.txt') as vocabulary_file:
+    for index, line in enumerate(vocabulary_file):
+        vocab_dict[index] = line
 
 # Finds marginal probability of each class
 for doc in xrange(num_train_docs):
@@ -61,20 +73,28 @@ for doc in xrange(num_test_docs):
 print 'scores[0, :] = ', scores[0, :]
 
 entropies = -np.sum(train_word_prob * np.log(train_word_prob), axis=0)
-sorted_words1 = np.argsort(entropies)
-sorted_words1 = sorted_words1[0:num_proto]
+sorted_words1 = np.argsort(entropies)[0:num_proto]
 
 variances = np.var(train_word_prob, axis=0)
-sorted_words2 = np.argsort(-variances)
-sorted_words2 = sorted_words2[0:num_proto]
+sorted_words2 = np.argsort(-variances)[0:num_proto]
 
 # Calculates performance of prototypes
 test_predictions = np.argmax(scores, axis=1)
 accuracy = np.sum(np.equal(test_predictions, test_labels)) / float(num_test_docs)
-print 'test_predictions[0:500] = ', test_predictions[0:500]
-print 'test_labels[0:500] = ', test_labels[0:500]
 print 'accuracy = ', accuracy
 
 # Prints a representative set of words in each class, by taking the words
 # with highest probability in each class from the set of prototypes
-reps = 0
+reps = np.zeros((num_labels, num_reps), dtype=np.uint16)
+for label in xrange(num_labels):
+    ctr = 0
+    for word in entropies:
+        if label == np.argmax(train_word_prob[:, word]):
+            reps[label, :] = word
+            ctr += 1
+        if ctr >= 10:
+            break
+
+print label_dict[0]
+for rep in xrange(num_reps):
+    print vocab_dict[reps[0, rep]]
