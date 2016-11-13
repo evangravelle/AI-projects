b5a855421b876c7e47d4c6b5013b762aedf726a0
Evan Gravelle <evangravelle@gmail.com>
2016-11-13 00:45:47 -0800

    now uses 4 frames

--- OpenAIGym/pong_better.py
+++ OpenAIGym/pong_better.py
@@ -33,10 +33,10 @@ epsilon_cutoff = 1000000
 num_episodes = 100  # per execution of script
 max_num_timesteps = 5000
 memory_cap = 10000  # One million should take up about 1GB of RAM
-batch_size = 1
+batch_size = 32
 gamma = 0.99
 # learning_rate = .00025  # assuming RMSProp is used
-learning_rate = .0001  # .02 was too fast, and .0001 also made Q diverge
+learning_rate = .001  # .02 was too fast, and .0001 also made Q diverge
 target_fix_time = 1000
 save_variables_time = 20000
 ep_range = 10
@@ -50,7 +50,7 @@ checkpoint_filename = 'pong_scores/model.ckpt'
 iteration_filename = 'pong_scores/iterations.txt'
 score_filename = 'pong_scores/score.txt'
 ep_filename = 'pong_scores/episodes.txt'
-Q_filename = 'pong_scores/Q_val.txt'
+Q_filename = 'pong_scores/avg_max_Q.txt'
 hold_out_filename = 'pong_scores/hold_out'
 replay_filename = 'pong_scores/replay.p'
 num_actions = env.action_space.n
@@ -58,11 +58,11 @@ num_rows = 210
 reduced_rows = 164
 num_cols = 160
 num_chan = 3
-input_size = 4 * reduced_rows * num_cols
-replay_memory = [np.zeros((memory_cap, input_size), dtype=np.int8),
+input_size = reduced_rows * num_cols
+replay_memory = [np.zeros((memory_cap, reduced_rows, num_cols, 4), dtype=np.int8),
                  np.zeros(memory_cap, dtype=np.uint8),
                  np.zeros(memory_cap, dtype=np.int8),
-                 np.zeros((memory_cap, input_size), dtype=np.int8)]
+                 np.zeros((memory_cap, reduced_rows, num_cols, 4), dtype=np.int8)]
 # print "size of replay_memory: ", sys.getsizeof(replay_memory)
 not_terminal = np.ones(memory_cap, dtype=int)
 np.set_printoptions(precision=4)
@@ -114,38 +114,35 @@ def epsilon_greedy(_epsilon, _vals):
     return int(_action)
 
 
-# TODO: HERE
-
 # INITIALIZE DEEP Q-NETWORK
 # He et al. recommend, for CNN with ReLUs, random Gaussian weights with zero mean and
 # std = sqrt(2.0/n), where n is number of input nodes
-s = tf.placeholder(tf.float32, shape=[None, input_size])  # 1st dim is batch size
+s = tf.placeholder(tf.float32, shape=[None, reduced_rows, num_cols, 4])  # 1st dim is batch size
 a = tf.placeholder(tf.int32, shape=[None])
 y = tf.placeholder(tf.float32, shape=[None])
 
-# First layer is max pooling to reduce the image to (?, 82, 80, 1)
-s_image = tf.reshape(s, [-1, reduced_rows, num_cols, 1])
-h_pool0 = -tf.nn.max_pool(-s_image, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-# print "h_pool0 = ", h_pool0
+# First layer is max pooling to reduce the image to (?, 82, 80, 4)
+h_pool0 = -tf.nn.max_pool(-s, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+print "h_pool0 = ", h_pool0
 
-# Second layer is 16 8x8 convolutions followed by ReLU (?, 21, 20, 16)
-W_conv1 = tf.Variable(tf.truncated_normal([8, 8, 1, 16], mean=0.0, stddev=tf.sqrt(2./64.)))
+# Second layer is 16 8x8 convolutions followed by ReLU (?, 21, 20, 64)
+W_conv1 = tf.Variable(tf.truncated_normal([8, 8, 4, 16], mean=0.0, stddev=tf.sqrt(2./64.)))
 b_conv1 = tf.Variable(tf.constant(0.0, shape=[16]))
 h_conv1 = tf.nn.relu(tf.nn.conv2d(h_pool0, W_conv1, strides=[1, 4, 4, 1], padding='SAME') + b_conv1)
-# print "h_conv1 = ", h_conv1
+print "h_conv1 = ", h_conv1
 
-# Third layer is 32 4x4 convolutions followed by ReLU (?, 11, 10, 32)
+# Third layer is 32 4x4 convolutions followed by ReLU (?, 11, 10, 128)
 W_conv2 = tf.Variable(tf.truncated_normal([4, 4, 16, 32], mean=0.0, stddev=tf.sqrt(2./16.)))
 b_conv2 = tf.Variable(tf.constant(0.0, shape=[32]))
 h_conv2 = tf.nn.relu(tf.nn.conv2d(h_conv1, W_conv2, strides=[1, 2, 2, 1], padding='SAME') + b_conv2)
-# print "h_conv2 = ", h_conv2
+print "h_conv2 = ", h_conv2
 
 # Fourth layer is fully connected layer followed by ReLU, with arbitrary choice of 256 neurons
-W_fc1 = tf.Variable(tf.truncated_normal([11 * 10 * 32, 256], mean=0.0, stddev=tf.sqrt(2.0/3520.)))
+W_fc1 = tf.Variable(tf.truncated_normal([11 * 10 * 32, 256], mean=0.0, stddev=tf.sqrt(2.0/(11*10*32))))
 b_fc1 = tf.Variable(tf.constant(0.0, shape=[256]))
 h_conv2_flat = tf.reshape(h_conv2, [-1, 11 * 10 * 32])
 h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
-# print "h_fc1 = ", h_fc1
+print "h_fc1 = ", h_fc1
 
 # Fifth layer is output
 # keep_prob = tf.placeholder(tf.float32)
@@ -153,7 +150,7 @@ h_fc1 = tf.nn.relu(tf.matmul(h_conv2_flat, W_fc1) + b_fc1)
 W_fc2 = tf.Variable(tf.truncated_normal([256, num_actions], mean=0.0, stddev=tf.sqrt(2.0/256.)))
 b_fc2 = tf.Variable(tf.constant(0.0, shape=[num_actions]))
 Q_vals = tf.matmul(h_fc1, W_fc2) + b_fc2
-# print "Q_vals = ", Q_vals
+print "Q_vals = ", Q_vals
 
 # Loss function is average (over mini-batch) mean squared error
 loss = tf.reduce_mean((y - tf.matmul(Q_vals, tf.transpose(tf.one_hot(a, num_actions)))) ** 2, reduction_indices=[1])
@@ -191,77 +188,56 @@ if os.path.isfile(hold_out_filename):
     hold_out_set = np.load(hold_out_filename)
     hold_out_length = np.shape(hold_out_set)[0]
 else:
-    hold_out_set = np.zeros((max_num_timesteps, input_size))
-    prev_obs = env.reset()
-    prev_obs_reduced = reduce_image(prev_obs)
+    hold_out_set = np.zeros((max_num_timesteps, reduced_rows, num_cols, 4))
+    hold_out_set[0, :, :, 0] = reduce_image(env.reset())
     action = env.action_space.sample()
-    obs, reward = env.step(action)[:2]
-    # env.render()
-    obs_reduced = reduce_image(obs)
-    obs_diff = obs_reduced - prev_obs_reduced
-    hold_out_set[0, :] = obs_diff.reshape((1, -1))
-    avg_Q = 0.
+    hold_out_set[0, :, :, 1] = reduce_image(env.step(action)[0])
+    action = env.action_space.sample()
+    hold_out_set[0, :, :, 2] = reduce_image(env.step(action)[0])
+    action = env.action_space.sample()
+    hold_out_set[0, :, :, 3] = reduce_image(env.step(action)[0])
+    avg_max_Q = 0.
     for t in range(1, max_num_timesteps):
-        prev_obs_reduced = obs_reduced[:]
-        prev_obs_diff = obs_diff[:]
         action = env.action_space.sample()
         obs, reward, done = env.step(action)[:3]
         # env.render()
         obs_reduced = reduce_image(obs)
-        obs_diff = obs_reduced - prev_obs_reduced
-        hold_out_set[t, :] = obs_diff.reshape((1, -1))
-        Q_vals_arr = sess1.run(Q_vals, feed_dict={s: hold_out_set[t, :].reshape(1, -1)})
-        avg_Q += np.amax(Q_vals_arr, axis=1)
+        hold_out_set[t, :, :, 0:3] = hold_out_set[t - 1, :, :, 1:4]
+        hold_out_set[t, :, :, 3] = obs_reduced
+        Q_vals_arr = sess1.run(Q_vals, feed_dict={s: hold_out_set[t, :, :, :].reshape(1, reduced_rows, num_cols, 4)})
+        avg_max_Q += np.amax(Q_vals_arr, axis=1)
         if done:
             break
     hold_out_length = t + 1
-    hold_out_set = hold_out_set[:hold_out_length, :]
+    hold_out_set = hold_out_set[:hold_out_length, :, :, :]
     np.save(hold_out_filename, hold_out_set)
     if not os.path.isfile(Q_filename):
-        avg_Q /= hold_out_length
+        avg_max_Q /= hold_out_length
         with open(Q_filename, 'w') as Q_file:
-            Q_file.write(str(float(avg_Q)) + '\n')
+            Q_file.write(str(float(avg_max_Q)) + '\n')
 
 
 # TRAINING LOOP
-avg_score = 0.
+avg_score = 0.0
 ep = start_ep
 while ep < start_ep + num_episodes:
     print "episode ", ep
-    prev_obs = env.reset()
-    prev_obs_reduced = reduce_image(prev_obs)
+    # Assumes no reward is obtained in first 4 frames
+    replay_ind = total_iter % memory_cap
+    replay_memory[0][replay_ind, :, :, 0] = reduce_image(env.reset())
     action = env.action_space.sample()
+    replay_memory[0][replay_ind, :, :, 1] = reduce_image(env.step(action)[0])
+    action = env.action_space.sample()
+    replay_memory[0][replay_ind, :, :, 2] = reduce_image(env.step(action)[0])
+    action = env.action_space.sample()
+    replay_memory[0][replay_ind, :, :, 3] = reduce_image(env.step(action)[0])
     obs, reward = env.step(action)[:2]
+    obs_reduced = reduce_image(obs)
     # env.render()
     avg_score += reward
-    obs_reduced = reduce_image(obs)
-    obs_diff = obs_reduced - reduce_image(prev_obs)
 
     for t in range(1, max_num_timesteps):
-        prev_obs_reduced = obs_reduced[:]
-        prev_obs_diff = obs_diff[:]
-        prev_Q_vals_toadd = sess1.run(Q_vals, feed_dict={s: prev_obs_diff.reshape((1, -1))})
-        action = epsilon_greedy(epsilon, prev_Q_vals_toadd)
-        obs, reward, done = env.step(action)[:3]
-        # env.render()
-        avg_score += reward
-        obs_reduced = reduce_image(obs)
-        obs_diff = obs_reduced - prev_obs_reduced
-        if verbose and t % 1 == 1:
-            plt.imshow(obs_diff, interpolation='nearest', cmap='Greys')
-            plt.show()
-        replay_ind = total_iter % memory_cap
-        if done:
-            not_terminal[replay_ind] = 0
-        else:
-            not_terminal[replay_ind] = 1
 
-        replay_memory[0][replay_ind, :] = prev_obs_diff.reshape(-1)
-        replay_memory[1][replay_ind] = action
-        replay_memory[2][replay_ind] = reward
-        replay_memory[3][replay_ind, :] = obs_diff.reshape(-1)
-
-        # print "replay_memory.size() = ", np.shape(replay_memory)
         current_batch_size = min([total_iter + 1, batch_size])
         # print "current_batch_size = ", current_batch_size
         current_replay_length = min([total_iter + 1, memory_cap])
@@ -269,22 +245,44 @@ while ep < start_ep + num_episodes:
         current_replays = random.sample(xrange(current_replay_length), current_batch_size)
         # print "current_replays = ", current_replays
 
+        Q_vals_for_action = sess1.run(Q_vals, feed_dict={
+          s: replay_memory[0][replay_ind, :, :, :].reshape(1, reduced_rows, num_cols, 4)})
+        action = epsilon_greedy(epsilon, Q_vals_for_action)
+        obs, reward, done = env.step(action)[:3]
+        # env.render()
+        avg_score += reward
+        obs_reduced = reduce_image(obs)
+        if verbose and t % 1 == 1:
+            plt.imshow(obs_reduced, interpolation='nearest', cmap='Greys')
+            plt.show()
+
+        if done:
+            not_terminal[replay_ind] = 0
+        else:
+            not_terminal[replay_ind] = 1
+
+        replay_memory[1][replay_ind] = action
+        replay_memory[2][replay_ind] = reward
+        replay_memory[3][replay_ind, :, :, 0:3] = replay_memory[0][replay_ind, :, :, 1:4]
+        replay_memory[3][replay_ind, :, :, 3] = obs_reduced
+
         # currently inefficient implementation, consider using partial_run (experimental)
         # Note, intermediate tensors are freed at the end of a sess.run()
         if verbose:
             prev_Q_vals_arr = sess1.run(Q_vals, feed_dict={
-              s: replay_memory[0][current_replays, :].reshape(current_batch_size, -1)})
+              s: replay_memory[0][current_replays, :, :, :].reshape(current_batch_size, reduced_rows, num_cols, 4)})
             Q_vals_arr = sess1.run(Q_vals, feed_dict={
-              s: replay_memory[3][current_replays, :].reshape(current_batch_size, -1)})
+              s: replay_memory[3][current_replays, :, :, :].reshape(current_batch_size, reduced_rows, num_cols, 4)})
 
         target_vals_arr = sess2.run(Q_vals, feed_dict={
-          s: replay_memory[3][current_replays, :].reshape(current_batch_size, -1)})
+          s: replay_memory[3][current_replays, :, :, :].reshape(current_batch_size, reduced_rows, num_cols, 4)})
         Q_max = np.amax(target_vals_arr, axis=1)
         r = replay_memory[2][current_replays]
         nt = not_terminal[current_replays]
         target = r + gamma * Q_max * nt
 
-        train_step.run(feed_dict={s: replay_memory[0][current_replays, :].reshape(current_batch_size, -1),
+        train_step.run(feed_dict={s: replay_memory[0][current_replays, :, :, :].reshape(
+                                    current_batch_size, reduced_rows, num_cols, 4),
                                   a: replay_memory[1][current_replays],
                                   y: target}, session=sess1)
 
@@ -296,7 +294,7 @@ while ep < start_ep + num_episodes:
             # if float(replay_memory[2][current_replays[ind]]) < -.5:
             if total_iter % 1 == 0:
                 prev_Q_vals_arr_after = sess1.run(Q_vals, feed_dict={
-                  s: replay_memory[0][current_replays, :].reshape(current_batch_size, -1)})
+                  s: replay_memory[0][current_replays, :, :, :].reshape(current_batch_size, reduced_rows, num_cols, 4)})
                 print 'total_iter = ', total_iter
                 print 'reward = ', replay_memory[2][current_replays[ind]]
                 print 'Q_vals_arr = ', Q_vals_arr[ind, :]
@@ -322,13 +320,16 @@ while ep < start_ep + num_episodes:
             if os.path.isfile(checkpoint_filename):
                 saver.restore(sess2, checkpoint_filename)
                 print 'Model restored to sess2 from', checkpoint_filename
-            avg_Q = 0.
-            hold_out_vals_arr = sess1.run(Q_vals, feed_dict={s: hold_out_set})
-            avg_Q = np.sum(np.amax(hold_out_vals_arr, axis=1)) / hold_out_length
+            avg_max_Q = 0.
+            hold_out_vals_arr = sess1.run(Q_vals, feed_dict={s: hold_out_set.reshape(-1, reduced_rows, num_cols, 4)})
+            avg_max_Q = np.sum(np.amax(hold_out_vals_arr, axis=1)) / hold_out_length
             with open(Q_filename, 'a') as Q_file:
-                Q_file.write(str(avg_Q) + '\n')
+                Q_file.write(str(avg_max_Q) + '\n')
 
         total_iter += 1
+        old_replay_ind = replay_ind
+        replay_ind = total_iter % memory_cap
+        replay_memory[0][replay_ind, :, :, :] = replay_memory[3][old_replay_ind, :, :, :]
         if total_iter <= epsilon_cutoff:
             epsilon = (epsilon_final - epsilon_initial) * total_iter / epsilon_cutoff + 1.0
         if done:
