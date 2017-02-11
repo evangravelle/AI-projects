ca84372fa50f63bb220daec5068efcde326805fb
evangravelle <evangravelle@gmail.com>
2017-02-10 23:26:55 -0800

    trees

--- PythonScripts/CodingInterview/Ch4Exercises.py
+++ PythonScripts/CodingInterview/Ch4Exercises.py
@@ -4,20 +4,46 @@
 class Node(object):
     """Creates a node."""
 
-    def __init__(self, next=None, data=None):
+    def __init__(self, data=None):
         self.data = data
-        self.next = next
+        self.left = None
+        self.right = None
 
 
-class BST(object):
+class BinarySearchTree(object):
     """Creates a binary search tree."""
 
     def __init__(self, data=None):
         self.root = Node(data=data)
 
     def add(self, data):
+        current = self.root
         if self.root.data is None:
-            self.root.data = data
+            traversing = False
         else:
-            pass
-            # compare, make children if they don't exist, etc.
+            traversing = True
+        while traversing:
+            if data < current.data and current.left is None:
+                current.left = Node(data=data)
+                current = current.left
+                traversing = False
+            elif data < current.data and current.left is not None:
+                current = current.left
+            elif data > current.data and current.right is None:
+                current.right = Node(data=data)
+                current = current.right
+                traversing = False
+            elif data > current.data and current.right is not None:
+                current = current.right
+        current.data = data
+
+
+    def BFS(self, data):
+        pass
+
+if __name__ == "__main__":
+    tree = BinarySearchTree()
+    tree.add(5)
+    tree.add(6)
+    tree.add(4)
+    print tree.root.left.data, tree.root.data, tree.root.right.data
