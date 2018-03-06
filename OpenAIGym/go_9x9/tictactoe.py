d3056c1e86fc27a099b461e464119e2bd5f5d3a2
Evan Gravelle <evangravelle@gmail.com>
2018-03-05 23:32:19 -0800

    update

--- OpenAIGym/go_9x9/tictactoe.py
+++ OpenAIGym/go_9x9/tictactoe.py
@@ -23,12 +23,12 @@ class Player(object):
 class TicTacToe(object):
     def __init__(self):
         self.state = list('         ')
-        self.player = Player()
+        self.players = Player()
         self.turn = 0
 
     def __str__(self):
-        return (self.board() + '\nTurn ' + str(self.turn + 1) + ', ' + self.player.players[self.player.player].upper() +
-                "\'s turn\n")
+        return (self.board() + '\nTurn ' + str(self.turn + 1) + ', ' +
+                self.players.current_player().upper() + "\'s turn\n")
 
     # Returns tuple of (is_terminal, reward)
     def check_state(self):
@@ -41,17 +41,17 @@ class TicTacToe(object):
                   self.state[0] + self.state[4] + self.state[8],
                   self.state[2] + self.state[4] + self.state[6]]
         if 'xxx' in threes:
-            return True, 1
+            return True, 1.0
         elif 'ooo' in threes:
-            return True, -1
+            return True, -1.0
         elif ' ' in self.state:
-            return False, 0
+            return False, 0.0
         else:
-            return True, 0
+            return True, 0.0
 
     def play(self, move):
         self.state[move] = self.current_player()
-        self.player.update_player()
+        self.players.update_player()
         self.turn += 1
 
     def reset(self):
@@ -67,7 +67,7 @@ class TicTacToe(object):
         return np.random.choice(self.get_moves())
 
     def current_player(self):
-        return self.player.current_player()
+        return self.players.current_player()
 
     def board(self):
         return (self.state[0] + ' | ' + self.state[1] + ' | ' + self.state[2] + '\n' + '---------' + '\n' +
@@ -81,15 +81,15 @@ class UCTTree(object):
         self.game = TicTacToe()
         self.nodes[self.game.get_state()]['N'] = 0
         for move in self.game.get_moves():
-            self.nodes[(self.game.get_state(), move)] = 0
+            self.nodes[(self.game.get_state(), move)]['Q'] = 0
         self.c_puct = .707
 
     def update_state(self, s, a, r):
-        self.nodes[(s, a)]['N'] += 1
+        self.nodes[(s, a)]['N'] += 1.0
         self.nodes[(s, a)]['Q'] += r
 
     def add_state(self, s, a):
-        self.nodes[(s, a)] = {'count': 1, 'value': 0}
+        self.nodes[(s, a)] = {'N': 1, 'Q': 0}
 
     def pick_move_uct(self):
         moves = self.game.get_moves()
@@ -100,7 +100,7 @@ class UCTTree(object):
         return moves[np.argmax(scores)[0]]
 
     def calc_score(self, s, a):
-        return self.nodes[(s, a)][]
+        return self.nodes[(s, a)]['Q']
 
     def tree_rollout(self):
         terminal, r = self.game.check_state()
@@ -110,18 +110,18 @@ class UCTTree(object):
                 r = self.tree_rollout()
         else:
             if not terminal:
-                self.add_state(s)
+                self.add_state(self.game.get_state())
                 self.game.play(self.game.pick_move_random())
                 r = self.default_rollout()
-        self.update_state(s, r)
-        return r
+        self.update_state(self.game.get_state(), r)
+        return self.game.get_state()['Q']
 
     def default_rollout(self):
         terminal, r = self.game.check_state()
         if not terminal:
             self.game.play(self.game.pick_move_random())
             r = self.default_rollout()
-        self.update_state(s, r)
+        self.update_state(self.game.check_state(), r)
         return r
 
     def search(self):
@@ -134,8 +134,8 @@ if __name__ == "__main__":
     for game in range(1):
         while not ttt.check_state()[0]:
             print(ttt)
-            move = np.random.choice(ttt.get_moves())
-            ttt.play(move)
+            next_move = np.random.choice(ttt.get_moves())
+            ttt.play(next_move)
         print(ttt.board())
         final_state = ttt.check_state()[1]
         if final_state == 1:
